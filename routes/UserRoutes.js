47b74e1af1aa44ccf7cfe32c642cb2b17b1ba2c5
fastcodingx <rakeshrauniyara1234@gmail.com>
2025-02-03 07:08:17 +0530

    get user controller added

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/UserController.js");
+const {
+  registerUser,
+  loginUser,
+  getUserDetails,
+} = require("../controllers/UserController.js");
 
 const router = express.Router();
 
 // Register route
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-
+router.post("/getuser", getUserDetails);
 
 module.exports = router;
